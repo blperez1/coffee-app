5c23a887c950e71b56b700874a383f3decd6da86
Glitch (hello-express) <none>
2021-02-02 23:35:03 -0600

    Add functionality to render html for each coffee

--- public/script.js
+++ public/script.js
@@ -1,41 +1,56 @@
 // client-side js, loaded by index.html
 // run by the browser each time the page is loaded
 
-console.log("hello world :o");
 
 // define variables that reference elements on our page
-const dreamsList = document.getElementById("dreams");
+const coffeesList = document.querySelector("#coffees");
 const dreamsForm = document.querySelector("form");
 
-// a helper function that creates a list item for a given dream
+// a helper function that creates renders html for each coffee
+function renderCoffee({name, roast, price}) {
+  return `
+          <h3>${name}</h3>
+          <p>${roast}</p>
+          <p>${price}</p>
+        `
+}
+
+function renderCoffees(coffees) {
+  let html = "";
+  for(let coffee of coffees) {
+    html += renderCoffee(coffee);
+  }
+  coffeesList.innerHTML = html;
+}
+
 function appendNewDream(dream) {
   const newListItem = document.createElement("li");
   newListItem.innerText = dream;
   dreamsList.appendChild(newListItem);
 }
 
-// fetch the initial list of dreams
-fetch("/dreams")
+
+fetch("/coffees")
   .then(response => response.json()) // parse the JSON from the server
-  .then(dreams => {
+  .then(coffees => {
     // remove the loading text
-    dreamsList.firstElementChild.remove();
+    coffeesList.firstElementChild.remove();
   
     // iterate through every dream and add it to our page
-    dreams.forEach(appendNewDream);
+    renderCoffees(coffees);
   
     // listen for the form to be submitted and add a new dream when it is
-    dreamsForm.addEventListener("submit", event => {
+    // dreamsForm.addEventListener("submit", event => {
       // stop our form submission from refreshing the page
-      event.preventDefault();
+      // event.preventDefault();
 
       // get dream value and add it to the list
-      let newDream = dreamsForm.elements.dream.value;
-      dreams.push(newDream);
-      appendNewDream(newDream);
+      // let newDream = dreamsForm.elements.dream.value;
+      // dreams.push(newDream);
+      // appendNewDream(newDream);
 
       // reset form
-      dreamsForm.reset();
-      dreamsForm.elements.dream.focus();
-    });
+      // dreamsForm.reset();
+      // dreamsForm.elements.dream.focus();
+    // });
   });
