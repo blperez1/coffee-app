9ef6a5da4c3b916a05e09bb4b000bfc9af7b468c
Glitch (hello-express) <none>
2021-02-02 23:34:04 -0600

    Add default object

--- server.js
+++ server.js
@@ -7,10 +7,21 @@ const express = require("express");
 const app = express();
 
 // our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
+const coffees = [
+  { id: 1, name: 'Light City', roast: 'light', price: 4.99},
+  { id: 2, name: 'Half City', roast: 'light', price: 4.99},
+  { id: 3, name: 'Cinnamon', roast: 'light', price: 4.99},
+  { id: 4, name: 'City', roast: 'medium', price: 4.99},
+  { id: 5, name: 'American', roast: 'medium', price: 4.99},
+  { id: 6, name: 'Breakfast', roast: 'medium', price: 4.99},
+  { id: 7, name: 'High', roast: 'dark', price: 4.99},
+  { id: 8, name: 'Continental', roast: 'dark', price: 4.99},
+  { id: 9, name: 'New Orleans', roast: 'dark', price: 4.99},
+  { id: 10, name: 'European', roast: 'dark', price: 4.99},
+  { id: 11, name: 'Espresso', roast: 'dark', price: 4.99},
+  { id: 12, name: 'Viennese', roast: 'dark', price: 4.99},
+  { id: 13, name: 'Italian', roast: 'dark', price: 4.99},
+  { id: 14, name: 'French', roast: 'dark', price: 4.99}
 ];
 
 // make all the files in 'public' available
@@ -18,14 +29,14 @@ const dreams = [
 app.use(express.static("public"));
 
 // https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
+app.get("/coffees", (req, res) => {
   // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+  res.json(coffees);
 });
 
 // listen for requests :)
